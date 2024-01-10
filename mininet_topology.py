86b13fda3338f8dc8b09cf25fbf408e2f4b74c48
Kamil Pokusa <kpokusa99@gmail.com>
2024-01-10 15:21:03 -0500

    Run our network with host and HTTP serwer

--- mininet_topology.py
+++ mininet_topology.py
@@ -1,29 +1,54 @@
-"""Custom topology example
-Two directly connected switches plus a host for each switch:
- host --- switch --- switch --- host
-Adding the 'topos' dict with a key/value pair to generate our newly defined
-topology enables one to pass in '--topo=mytopo' from the command line.
-"""
-from mininet.topo import Topo
-class MyTopo( Topo ):
-    "Simple topology example."
-     def init( self ):
-        "Create custom topo."
-        # Initialize topology
-        Topo.init( self )
-        # Add hosts and switches
-        Host = self.addHost( 'h1' )
-        serv1 = self.addHost( 'serv1' )
-        serv2 = self.addHost( 'serv2' )
-        attacerHost1 = self.addHost( 'a1' )
+from mininet.net import Mininet
+from mininet.node import Controller, OVSSwitch
+from mininet.cli import CLI
 
-        Switch = self.addSwitch( 's1' )
+import sys
 
+def create_sdn_network(controller_ip='127.0.0.1', controller_port=6633):
+    # Create Mininet object
+    net = Mininet(controller=Controller, switch=OVSSwitch)
 
-        # Add links
-        self.addLink( serv1, Switch)
-        self.addLink( serv2, Switch)
-        self.addLink( Host, Switch)
-        self.addLink( attacerHost1, Switch)
+    # Add external controller if specified
+    if controller_ip != '127.0.0.1':
+        c0 = net.addController('c0', controller=Controller, ip=controller_ip, port=controller_port)
+    else:
+        c0 = net.addController('c0')
 
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+    # Add OpenFlow switch
+    s1 = net.addSwitch('s1')
+
+    # Add four hosts
+    h1 = net.addHost('h1')
+    h2 = net.addHost('h2')
+    h3 = net.addHost('h3')
+    h4 = net.addHost('h4')
+
+    # Connect hosts to the switch
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+    net.addLink(h3, s1)
+    net.addLink(h4, s1)
+
+    # Start the network
+    net.build()
+    c0.start()
+    s1.start([c0])
+
+    # Configure host interfaces
+    h1.cmd('ifconfig h1-eth0 10.0.0.1 netmask 255.255.255.0')
+    h2.cmd('ifconfig h2-eth0 10.0.0.2 netmask 255.255.255.0')
+    h3.cmd('ifconfig h3-eth0 10.0.0.3 netmask 255.255.255.0')
+    h4.cmd('ifconfig h4-eth0 10.0.0.4 netmask 255.255.255.0')
+
+    # Start HTTP server on h1 and h2
+    h1.cmd('python -m SimpleHTTPServer 80 &')
+    h2.cmd('python -m SimpleHTTPServer 80 &')
+
+    # Start Mininet CLI for interactive control
+    CLI(net)
+
+    # Stop the network after completion
+    net.stop()
+
+if __name__ == '__main__':
+    create_sdn_network()
